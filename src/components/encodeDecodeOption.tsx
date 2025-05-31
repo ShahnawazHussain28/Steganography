8fa6f2690186d9a06c12682fa3a157c121bfc2ad
Shahnawaz Hussain <shahnawazhussain2802@gmail.com>
2025-05-31 19:18:29 +0530

    feat: decode section UI

--- src/components/encodeDecodeOption.tsx
+++ src/components/encodeDecodeOption.tsx
@@ -6,6 +6,9 @@ export default function EncodeDecodeOption() {
   const { operation, setOperation } = useSteganoContext()
   const [usePassword, setUsePassword] = useState(false)
   const [showPassword, setShowPassword] = useState(false)
+  const [requirePassword, setRequirePassword] = useState(true)
+  const [decodedText, setDecodedText] = useState("")
+
   return (
     <div
       className={`flex flex-col bg-[#191738] transition duration-200 rounded-lg ${
@@ -98,6 +101,61 @@ export default function EncodeDecodeOption() {
         >
           Decode Message
         </button>
+        {operation === "DECODE" && (
+          <div className="p-4">
+            <p>
+              The message is encrypted with a password. Please enter the
+              password below
+            </p>
+            {requirePassword && (
+              <div className="w-full mt-4 relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  maxLength={20}
+                  className="p-2 pl-4 w-full three-d focus:outline-none"
+                  placeholder="Enter the password"
+                />
+                {!showPassword ? (
+                  <FaRegEye
+                    onClick={() => setShowPassword(true)}
+                    className="absolute right-0 top-1/2 -translate-y-1/2 mr-4 text-2xl hover:text-gray-400 cursor-pointer transition"
+                  />
+                ) : (
+                  <FaRegEyeSlash
+                    onClick={() => setShowPassword(false)}
+                    className="absolute right-0 top-1/2 -translate-y-1/2 mr-4 text-2xl hover:text-gray-400 cursor-pointer transition"
+                  />
+                )}
+              </div>
+            )}
+            <div className="flex justify-between items-center mt-6">
+              <button
+                id="encode"
+                className="py-3 px-6 three-d cursor-pointer !bg-[#29265c] !rounded-full"
+              >
+                Decode Message
+              </button>
+              <button
+                onClick={() => setOperation(null)}
+                className="mr-4 cursor-pointer hover:bg-red-500/50 py-2 px-4 rounded-full"
+              >
+                Start Over
+              </button>
+            </div>
+            <div className="relative">
+              <textarea
+                className="w-full p-2 three-d mt-8"
+                id="message-input"
+                rows={4}
+                disabled
+                value={decodedText}
+                onClick={(e) => {
+                  navigator.clipboard.writeText(decodedText)
+                }}
+              ></textarea>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
